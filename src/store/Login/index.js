efa8956b2f050d4955be20372df3a158db5d8954
wliu044 <86115967+wliu044@users.noreply.github.com>
2023-02-13 11:39:49 +0800

    Revert "Update index.js"
    This reverts commit 09c1a3d67d31642bf488ba0d63a43231ec01b201.

--- src/store/Login/index.js
+++ src/store/Login/index.js
@@ -1,67 +1,67 @@
-import { reqUserLogin, reqUserInfo, reqUserLogout } from "@/API";
 
-const actions = {
-    // Login request
-    async userLogin(context, data) {
-        console.log(data);
-        let res = await reqUserLogin(data)
-        console.log(res);
-        if (res.data.code == 200) {
-            context.commit('USERLOGIN', res.data)
-            return 'ok'
-        } else {
-            return Promise.reject(new Error('Incorrect username or password'))
-        }
+    import { reqUserLogin,reqUserInfo,reqUserLogout } from "@/API";
+const actions={
+    //登入请求
+   async userLogin(context,data){
+    console.log(data);
+      let res = await reqUserLogin(data)
+      console.log(res);
+      if(res.data.code == 200){
+        context.commit('USERLOGIN',res.data)
+        return 'ok'
+      }else{
+        return Promise.reject(new Error('账号密码有误'))
+      }
     },
-    // User information returned from login
-    async getUserInfo({commit}) {
-        let res = await reqUserInfo()
-        if (res.data.code == 200) {
-            commit('GETUSERINFO', res.data)
-            return 'ok'
-        } else {
-            return Promise.reject(new Error('Failed to return user information'))
-        }
+    //登入返回的用户信息
+   async getUserInfo({commit}){
+       let res = await reqUserInfo()
+       if(res.data.code==200){
+        commit('GETUSERINFO',res.data)
+        return 'ok'
+       }else{
+        return Promise.reject(new Error('返回用户信息失败'))
+       }
+
     },
-    // Logout request, notify mutations to delete user login data
-    async logout({commit}) {
-        let res = await reqUserLogout()
-        if (res.data.code == 200) {
-            commit('LOGOUT')
-            return 'ok'
-        } else {
-            return Promise.reject(new Error('Logout failed'))
-        }
+    // 退出登入的请求，通知mutations删除用户登入数据
+   async logout({commit}){
+      let res = await reqUserLogout()
+      if(res.data.code == 200){
+        commit('LOGOUT')
+        return 'ok'
+      }else{
+        return Promise.reject(new Error('退出登入失败'))
+      }
     }
+    
 }
-
 const mutations = {
-    USERLOGIN(state, value) {
+    USERLOGIN(state,value){
         state.data = value.data
-        // After logging in, it will jump to the main page, before jumping, store the user token in vuex in local storage
-        localStorage.setItem('userToken', state.data.token)
+         // 登入完毕后会跳转到主页面，跳转之前把vuex中的用户token存在本地存储中
+      
+         localStorage.setItem('userToken',state.data.token)
+  
     },
-    GETUSERINFO(state, value) {
+    GETUSERINFO(state,value){
         state.userInfo = value.data
     },
-    LOGOUT(state) {
+    LOGOUT(state){
         state.data = {}
         state.userInfo = {}
         localStorage.removeItem('userToken')
     }
 }
-
 const state = {
-    data: {},
-    userInfo: {},
-    token: localStorage.getItem('userToken') || ''
+    data:{},
+    userInfo:{},
+    token:localStorage.getItem('userToken') || ''
 }
-
 const getters = {
-
+ 
 }
-
-export default {
+export default{
     actions,
     mutations,
     state,
